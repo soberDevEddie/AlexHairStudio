def85cf872d6b09a447783359d28f444e24dbefd
EddieO23 <eocampo52@gmail.com>
2026-08-07 14:27:30 -0500

    init commit

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import Hero from './components/Hero';
+import Services from './components/Services';
+import Gallery from './components/Gallery';
+import Pricing from './components/Pricing';
+import Team from './components/Team';
+import Testimonials from './components/Testimonials';
+import Booking from './components/Booking';
+import Footer from './components/Footer';
+
+function App() {
+  return (
+    <>
+      <Hero />
+      <Services />
+      <Gallery />
+      <Pricing />
+      <Team />
+      <Testimonials />
+      <Booking />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
