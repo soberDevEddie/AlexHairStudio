cfd9e2e4883e6fa73e74ddfe9ccb331f4cb84f94
EddieO23 <eocampo52@gmail.com>
2026-08-08 13:03:23 -0500

    added bootstrap to project

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -2,7 +2,10 @@ import React from 'react'
 
 const Hero = () => {
   return (
-    <div>Hero</div>
+    <div className='vh-50 d-flex justify-content-center align-items-center text-center'>
+      <h1>Welcome to Alex's Hair Studio  </h1>
+      <p className="text-lead">Get the look you've always wanted with our expert stylists.</p>
+    </div>
   )
 }
 
